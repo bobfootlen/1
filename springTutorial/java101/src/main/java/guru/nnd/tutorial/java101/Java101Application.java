2d166d8d69a14ba6ef7c34ccafab8eba5f95b04e
Phillip McKeen <pmckeen@gmail.com>
2024-09-10 16:33:43 -0700

    Add add handler

--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/Java101Application.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/Java101Application.java
@@ -2,18 +2,26 @@ package guru.nnd.tutorial.java101;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
 public class Java101Application {
 
+	@RequestMapping("/add")
+	public String add(@RequestParam("operands") double[] operands) {
+		double value = 0;
+		for (var operand : operands) {
+			value += operand;
+		}
+		return String.valueOf(value);
+	}
 
 	@RequestMapping("/helloWorld")
 	public String home() {
-	  return "Hello Docker World";
+		return "Hello Docker World";
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Java101Application.class, args);
 	}
