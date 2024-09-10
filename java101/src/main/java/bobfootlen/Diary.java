5dd8b1b26698ec5684242ebfacfe4b71926362aa
bobfootlen <foot49900@gmail.com>
2024-09-10 15:48:38 -0700

    imports

--- java101/src/main/java/bobfootlen/Diary.java
+++ java101/src/main/java/bobfootlen/Diary.java
@@ -1,7 +1,16 @@
 package bobfootlen;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Diary {
     public static void main(String[] nope) {
+         try (var scanner = new Scanner(System.in)) {
+            var lines = new ArrayList<String>();
+            var line = scanner.nextLine();
+            lines.add(line);  
+         
+        }
         
     }
 }
