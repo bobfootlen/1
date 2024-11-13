c661a6c04221c2754995523e33ef1bc97170c941
bobfootlen <foot49900@gmail.com>
2024-11-12 16:11:17 -0800

    added extends to code

--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/SocketConfig.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/SocketConfig.java
@@ -3,7 +3,7 @@ package guru.nnd.tutorial.java101;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
-public class SocketConfig implements WebSocketConfigurer{
+public class SocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
