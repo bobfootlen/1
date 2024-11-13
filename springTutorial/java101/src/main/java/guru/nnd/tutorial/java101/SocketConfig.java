3c938bb4c52e36058445205ca2da086ae704d37e
Phillip McKeen <pmckeen@gmail.com>
2024-11-12 16:11:15 -0800

    Register PlayerHandler

--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/SocketConfig.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/SocketConfig.java
@@ -1,14 +1,19 @@
 package guru.nnd.tutorial.java101;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
+@Configuration
+@EnableWebSocket
 public class SocketConfig implements WebSocketConfigurer{
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'registerWebSocketHandlers'");
+        
+        registry.addHandler(new PlayerHandler(), "/player-connect");
+        
     }
 
 }
