262b4f557a5b5ed591a5ea877e3ad6738c95167e
Phillip McKeen <pmckeen@gmail.com>
2024-11-12 16:31:54 -0800

    Format

--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/PlayerHandler.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/PlayerHandler.java
@@ -8,21 +8,23 @@ import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-public class PlayerHandler extends TextWebSocketHandler{
- private List<WebSocketSession> clients = new ArrayList<>();
+public class PlayerHandler extends TextWebSocketHandler {
+    private List<WebSocketSession> clients = new ArrayList<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         clients.add(session);
         String role = assignRole(clients.size());
-        
+
         // Send role to the client
         session.sendMessage(new TextMessage("role:" + role));
     }
 
     private String assignRole(int connectionOrder) {
-        if (connectionOrder == 1) return "x";
-        if (connectionOrder == 2) return "o";
+        if (connectionOrder == 1)
+            return "x";
+        if (connectionOrder == 2)
+            return "o";
         return "spectator";
     }
 
