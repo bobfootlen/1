28d56247276fd6ac1edbc13cc680f4e86bca03ac
bobfootlen <foot49900@gmail.com>
2024-12-10 16:07:40 -0800

    \u00a00 to

--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/Java101Application.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/Java101Application.java
@@ -19,7 +19,7 @@ public class Java101Application {
 	public String resetbord() {
 		for (var i = 0; i < 3; i++) {
 			for (var j = 0; j < 3; j++) {
-				bord[i][j] = '\u00a0';
+				bord[i][j] = ' ';
 			}
 		}
 
@@ -28,18 +28,50 @@ public class Java101Application {
 
 	@PostMapping("/tictac/bord")
 	public ResponseEntity<String> recordmove(String player, int column, int row) {
-		if(currentPlayer != player.charAt(0)){
+		if (currentPlayer != player.charAt(0)) {
 			return new ResponseEntity<>("It's not your turn.", HttpStatus.BAD_REQUEST);
 		}
 		if (column > 2 || column < 0 || row > 2 || row < 0) {
 			return new ResponseEntity<>("Row or Column Out of Bounds", HttpStatus.BAD_REQUEST);
 		}
-		if (bord[row][column] != '\u00A0') {
+		if (bord[row][column] != ' ') {
 			return new ResponseEntity<>("Space Already Occupied", HttpStatus.BAD_REQUEST);
 		}
 		bord[row][column] = currentPlayer;
-		currentPlayer = currentPlayer == 'x' ? 'o' : 'x';
+		var endgame = checkEndGame();
+		if (endgame == null)
+			currentPlayer = currentPlayer == 'x' ? 'o' : 'x';
+		else {
+			System.out.println(endgame);
+			currentPlayer = ' ';
+		}
 		return new ResponseEntity<>(getbord(), HttpStatus.OK);
+	}
+
+	private String checkEndGame() {
+
+		for (var i = 0; i < 3; i++) {
+
+			if (checkSet(bord[i][0], bord[i][1], bord[i][2]))
+				return "" + bord[i][0] + " Wins";
+		}
+		for (var i = 0; i < 3; i++) {
+
+			if (checkSet(bord[0][i], bord[1][i], bord[2][i]))
+				return "" + bord[0][i] + " Wins";
+		}
+		if (checkSet(bord[0][0], bord[1][1], bord[2][2]))
+			return "" + bord[0][0] + " Wins";
+		if (checkSet(bord[2][0], bord[1][1], bord[0][2]))
+			return "" + bord[2][0] + " Wins";
+		if (getbord().contains(" "))
+			return null;
+		else
+			return "Cat's Game!";
+	}
+
+	private boolean checkSet(char c1, char c2, char c3) {
+		return c1 != ' ' && c1 == c2 && c1 == c3;
 	}
 
 	@GetMapping("/tictac/player")
@@ -58,9 +90,9 @@ public class Java101Application {
 	}
 
 	private char[][] bord = {
-			{ '\u00A0', '\u00A0', '\u00A0' },
-			{ '\u00A0', '\u00A0', '\u00A0' },
-			{ '\u00A0', '\u00A0', '\u00A0' }
+			{ ' ', ' ', ' ' },
+			{ ' ', ' ', ' ' },
+			{ ' ', ' ', ' ' }
 	};
 
 	private char currentPlayer = 'x';
--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/TicTacSocketHandler.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/TicTacSocketHandler.java
@@ -3,5 +3,5 @@ package guru.nnd.tutorial.java101;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class TicTacSocketHandler extends TextWebSocketHandler {
-    
+
 }
