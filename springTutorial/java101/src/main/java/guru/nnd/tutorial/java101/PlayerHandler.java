780202f24ffa35a694f1832475ac5f9c53c7ce8a
Phillip McKeen <pmckeen@gmail.com>
2025-01-21 16:30:50 -0800

    Fix player-state handling

--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/PlayerHandler.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/PlayerHandler.java
@@ -1,7 +1,9 @@
 package guru.nnd.tutorial.java101;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
@@ -9,27 +11,34 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class PlayerHandler extends TextWebSocketHandler {
+
     private List<WebSocketSession> clients = new ArrayList<>();
+
+    private Map<WebSocketSession,String> players = new HashMap<>();
 
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
         clients.add(session);
-        String role = assignRole(clients.size());
+        String role = assignRole(session);
 
         // Send role to the client
         session.sendMessage(new TextMessage("role:" + role));
     }
 
-    private String assignRole(int connectionOrder) {
-        if (connectionOrder == 1)
-            return "x";
-        if (connectionOrder == 2)
-            return "o";
-        return "spectator";
+    private String assignRole(WebSocketSession session) {
+        if(!players.containsValue("x")){
+            players.put(session, "x");
+        }else if(!players.containsValue("o"){
+            players.put(session,"o");
+        }else{
+            players.put(session,"spectator");
+        }
+        return players.get(session);
     }
 
     @Override
     public void afterConnectionClosed(WebSocketSession session, CloseStatus status) {
+        players.remove(session);
         clients.remove(session); // Clean up after disconnect
     }
 }
