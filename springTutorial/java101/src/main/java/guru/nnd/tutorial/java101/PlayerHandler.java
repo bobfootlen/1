430c3c8e84f40b4d1534a7444904b0291a212317
bobfootlen <foot49900@gmail.com>
2025-02-04 17:23:14 -0800

    hendle messages

--- springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/PlayerHandler.java
+++ springTutorial/java101/src/main/java/guru/nnd/tutorial/java101/PlayerHandler.java
@@ -1,10 +1,14 @@
 package guru.nnd.tutorial.java101;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonGetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.web.socket.CloseStatus;
@@ -14,6 +18,7 @@ import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 public class PlayerHandler extends TextWebSocketHandler {
 
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
     private List<WebSocketSession> clients = new ArrayList<>();
 
     private class GameState {
@@ -43,9 +48,13 @@ public class PlayerHandler extends TextWebSocketHandler {
 
         // Send role to the client
         session.sendMessage(new TextMessage("role:" + role));
+        sendState();
+    }
+
+    private void sendState() throws IOException, JsonProcessingException {
         for (var client : clients) {
             client.sendMessage(new TextMessage(
-                    "state:" + new ObjectMapper()
+                    "state:" + OBJECT_MAPPER
                             .writeValueAsString(gameState)));
         }
     }
@@ -62,11 +71,86 @@ public class PlayerHandler extends TextWebSocketHandler {
                 // handle "Sample" message type
                 System.out.println("Handle Sample");
                 break;
+            case "move":
+                // handle "move" message type
+                handlemove(session, messageData);
+                break;
+            case "restart":
+                // handle "Sample" message type
+                System.out.println("Handle Sample");
+                break;
             default:
                 session.sendMessage(new TextMessage("error:Unhandled Message Type."));
         }
     }
 
+    
+    private class MoveRequest{
+        @JsonProperty
+        private int row;
+        @JsonProperty
+        private int column;
+    }
+
+    private void handlemove(WebSocketSession session, String payload) throws IOException{
+        if (gameState.currentPlayer != players.get(session).charAt(0)) {
+			session.sendMessage(new TextMessage("error:Not your turn!"));
+            return;
+		}
+        var request = OBJECT_MAPPER.readValue(payload, MoveRequest.class);
+		if (request.column > 2 || request.column < 0 || request.row > 2 || request.row < 0) {
+            session.sendMessage(new TextMessage("error:Row or Column Out of Bounds"));
+			return;
+		}
+		if (gameState.board[request.row][request.column] != ' ') {
+            session.sendMessage(new TextMessage("error:Space Already Occupied"));
+			return;
+		}
+        gameState.board[request.row][request.column] = gameState.currentPlayer;
+		var endgame = checkEndGame();
+		if (endgame == null)
+            gameState.currentPlayer = gameState.currentPlayer == 'x' ? 'o' : 'x';
+		else {
+            gameState.winState = endgame;
+			
+			gameState.currentPlayer = ' ';
+		}
+        sendState();
+    }
+    private String checkEndGame() {
+var board = gameState.board;
+		for (var i = 0; i < 3; i++) {
+
+			if (checkSet(board[i][0], board[i][1], board[i][2]))
+				return "" + board[i][0] + " Wins";
+		}
+		for (var i = 0; i < 3; i++) {
+
+			if (checkSet(board[0][i], board[1][i], board[2][i]))
+				return "" + board[0][i] + " Wins";
+		}
+		if (checkSet(board[0][0], board[1][1], board[2][2]))
+			return "" + board[0][0] + " Wins";
+		if (checkSet(board[2][0], board[1][1], board[0][2]))
+			return "" + board[2][0] + " Wins";
+		if (boardToString(board).contains(" "))
+			return null;
+		else
+			return "Cat's Game!";
+	}
+
+    private boolean checkSet(char c1, char c2, char c3) {
+		return c1 != ' ' && c1 == c2 && c1 == c3;
+	}
+    private String boardToString(char[][] board) {
+		StringBuilder builder = new StringBuilder(11);
+		builder.append('"');
+		builder.append(board[0]);
+		builder.append(board[1]);
+		builder.append(board[2]);
+		builder.append('"');
+		return builder.toString();
+	}
     private String assignRole(WebSocketSession session) {
         if (!players.containsValue("x")) {
             players.put(session, "x");
